ab0e83ec063079f0c6305f30ebcda91e452b0f38
Haris M. Bhatti <h.mbhatti@ymail.com>
2011-07-10 23:52:00 +0100

    test

--- doit.d
+++ doit.d
@@ -1,10 +1,21 @@
 module DoIt;
 
 import std.stdio;
-import core.vararg;
+import std.getopt;
+import Options;
 
-int main()
+int main(string[] args)
 {
+	Options command_line;
+
+	getopt(args,
+		"categories", &command_line.m_list_categories);
+
+	if (command_line.m_list_categories)
+	{
+		writeln("Listing categories");
+	}
+
 	writeln("lets do this.");
 	return 0;
 }
